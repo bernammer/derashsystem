d972d2334fd20b5c10c94b22cc79df2726557a42
Abdisa T. <abdtsegaye@gmail.com>
2024-04-06 01:47:39 +0300

    added employee sign in

--- client-web-app/src/App.jsx
+++ client-web-app/src/App.jsx
@@ -1,17 +1,7 @@
-import React, {
-    useEffect
-} from 'react';
-import {
-    Navigate,
-    Route,
-    Routes,
-    useLocation
-} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
 
-import {
-    Bounce,
-    ToastContainer
-} from "react-toastify";
+import {Bounce, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 import './css/style.css';
@@ -22,9 +12,7 @@ import SignUp from "./pages/authentication/SignUp";
 import SignIn from "./pages/authentication/SignIn";
 import Overview from "./pages/features/dashboard";
 
-import {
-    useGetCurrentUserQuery
-} from "./api/apiSlice";
+import {useGetCurrentUserQuery} from "./api/apiSlice";
 import HomePage from "./pages/features/home";
 
 import Users from './pages/features/users'
@@ -37,9 +25,7 @@ import Roles from './pages/features/roles'
 import RolesAdd from './pages/features/roles/RolesAdd'
 import RolesList from './pages/features/roles/RolesList'
 import RolesListItemDetail from './pages/features/roles/RolesListItemDetail'
-
-
-
+import EmployeeSignIn from "./pages/authentication/EmployeeSignIn";
 
 
 function App() {
@@ -61,33 +47,36 @@ function App() {
     return (
         <>
             {/*TODO: improve this shitty logic and compartmentalize it */}
-            {isError && error.status === 401 && (location.pathname !== '/signin' && location.pathname !== '/signup' && location.pathname !== '/reset-password') ?
-                <Navigate to='/signin' replace={true}/> : <></>}
+            {isError &&
+            error.status === 401 &&
+            (location.pathname !== '/signin'
+                && location.pathname !== '/employee-signin'
+                && location.pathname !== '/signup'
+                && location.pathname !== '/reset-password')
+                ? <Navigate to='/employee-signin' replace={true}/> : <></>}
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/" element={<MainDashboard/>}>
                     <Route path="/dashboard" element={<Overview/>}/>
-                    
-                    
-                    <Route path="/users" element={<Users />}>
-                        <Route path="/users/add" element={<UsersAdd />} />
-<Route path="/users" element={<UsersList />} />
-<Route path="/users/:userId" element={<UsersListItemDetail />} />
+
+
+                    <Route path="/users" element={<Users/>}>
+                        <Route path="/users/add" element={<UsersAdd/>}/>
+                        <Route path="/users" element={<UsersList/>}/>
+                        <Route path="/users/:userId" element={<UsersListItemDetail/>}/>
 
                     </Route>
-                    
 
-                    <Route path="/roles" element={<Roles />}>
-                        <Route path="/roles/add" element={<RolesAdd />} />
-<Route path="/roles" element={<RolesList />} />
-<Route path="/roles/:roleId" element={<RolesListItemDetail />} />
+                    <Route path="/roles" element={<Roles/>}>
+                        <Route path="/roles/add" element={<RolesAdd/>}/>
+                        <Route path="/roles" element={<RolesList/>}/>
+                        <Route path="/roles/:roleId" element={<RolesListItemDetail/>}/>
 
                     </Route>
-                    
-         
 
                 </Route>
                 <Route path="/signin" element={<SignIn/>}/>
+                <Route path="/employee-signin" element={<EmployeeSignIn/>}/>
                 <Route path="/signup" element={<SignUp/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
